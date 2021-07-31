162631104598d74207041c2a52c8c863e25d08bd
sachinABK <57856730+sachinABK@users.noreply.github.com>
2021-07-31 22:51:29 +0530

    adding features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { featchWeather } from "./api/fetchWeather";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const test = document.querySelector("#testfavicon");
+
+  const testFavicon = () => {
+    const favicon = test;
+    favicon.href =
+      "https://cdn.iconscout.com/icon/free/png-512/weather-296-1100758.png";
+  };
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await featchWeather(query);
+      console.log(data);
+      setWeather(data);
+      setQuery("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <img src={testFavicon()} alt="" />
+      <input
+        type="text"
+        className="search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt=""
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
